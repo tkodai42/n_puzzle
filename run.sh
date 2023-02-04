a71bcee9fbbc80aa6086e72f4a3c51029f84f276
Tagawa Kodai <tkodai42@gmail.com>
2023-02-04 16:57:36 +0900

    Add: makefile option -O2

--- run.sh
+++ run.sh
@@ -8,6 +8,10 @@ fi
 
 RESULT_DIR=result
 mkdir result
+
+make -C ./use_close_set
+make -C ./use_open_status_vector
+
 python ./use_close_set/tester/gen.py $NUM > ./$RESULT_DIR/test_file
 ./use_close_set/n_puzzle -f5 -v ./$RESULT_DIR/test_file > ./$RESULT_DIR/result_set
 ./use_open_status_vector/n_puzzle -f5 -v ./$RESULT_DIR/test_file > ./$RESULT_DIR/result_vec
